9ce2b1856de7c68c10062c10ef2e41a22f73b55a
Stiveys <stevomwaura@gmail.com>
2025-02-12 09:45:01 +0300

    new

--- lib/not_none_functions.py
+++ lib/not_none_functions.py
@@ -1,2 +1,4 @@
-def return_not_none():
-    return None
+# lib/not_none_functions.py
+
+def your_function():
+    return "This is not None"
--- lib/testing/test_not_none.py
+++ lib/testing/test_not_none.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python3
+# lib/testing/test_not_none.py
 
-from not_none_functions import return_not_none
-
-# def test_return_not_none():
-#     '''in not_none_functions, function "return_not_none" returns a value that is not None.'''
-#     assert False
+def test_not_none():
+    from not_none_functions import your_function  # Import the function you want to test
+    result = your_function()  # Call the function
+    assert result is not None  # Ensure the result is not None
